ff92b406b218ef73c023f8378166ce6f303ac2ff
Alex Gutierrez <agutierrez@campus.udes.edu.co>
2016-11-30 17:54:16 -0500

    + Added functions for register and edit retrospectives by user. + Adjusted login and logout functions.

--- retrospective/urls.py
+++ retrospective/urls.py
@@ -4,5 +4,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^retrospective/(?P<id_retro>[-_\w]+)/$',
+        views.RetrospectiveDetailView.as_view(), name="retrospective"),
+    url(r'^retrospective/create/(?P<id_retro>[-_\w]+)/$',
+        views.RetrospectiveUserCreateView.as_view(),
+        name='retrospective-create'),
+    url(r'^retrospective/edit/(?P<id_retro>[-_\w]+)/$',
+        views.RetrospectiveUserEditView.as_view(),
+        name='retrospective-edit'),
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
+    url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
 ]
--- retrospective/views.py
+++ retrospective/views.py
@@ -7,32 +7,55 @@ from django.contrib.auth import login, logout, authenticate, \
     update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, \
     RedirectView, View, FormView
 from django.views.generic.edit import UpdateView
+from .forms import LoginForm
+from .models import Planning, Retrospective, RetrospectiveUser
+from django.contrib.auth.models import User
 
 
-class IndexView(View):
+class IndexView(View, LoginRequiredMixin):
     template = "retrospective/index.html"
+    login_url = 'login'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template, {})
+        plannings = Planning.objects.all()
+        retrospectives = Retrospective.objects.all()
 
-class LoginView(FormView):
-    form_class = AuthenticationForm
-    template_name = "auth/login.html"
-    success_url = reverse_lazy("home")
+        context = {
+            'plannings': plannings,
+            'retrospectives': retrospectives
+        }
+        return render(request, self.template, context)
 
-    def dispatch(self, request, *args, **kwargs):
+
+class LoginView(View):
+    form = LoginForm()
+    message = None
+    template = "auth/login.html"
+    success_url = reverse_lazy("index")
+
+    def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
-            return HttpResponseRedirect(self.get_success_url())
-        else:
-            return super(LoginView, self).dispatch(request, *args, **kwargs)
+            return redirect('index')
 
-    def form_valid(self, form):
-        login(self.request, form.get_user())
-        return super(LoginView, self).form_valid(form)
+        return render(request, self.template, self.get_context())
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            self.message = 'Usuario o password incorrectos'
+        return render(request, self.template, self.get_context())
+
+    def get_context(self):
+        return {'form': self.form, 'message': self.message}
 
 
 class LogoutView(RedirectView):
@@ -43,11 +66,50 @@ class LogoutView(RedirectView):
         return super(LogoutView, self).get(request, *args, **kwargs)
 
 
-class LoginRequiredMixin(object):
+class RetrospectiveDetailView(DetailView, LoginRequiredMixin):
+    model = Retrospective
+    template = "retrospective/retrospective_detail.html"
+    slug_field = "id"
+    slug_url_kwarg = "id_retro"
+    login_url = 'login'
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated():
-            return HttpResponseRedirect(reverse('login'))
-        else:
-            return super(
-                LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(RetrospectiveDetailView, self).get_context_data(**kwargs)
+        data = RetrospectiveUser.objects.filter(
+            retrospective=self.object
+        )
+        data_off = User.objects.exclude(
+            id__in=RetrospectiveUser.objects.values_list('id', flat=True)
+        )
+        context['data'] = data
+        context['data_off'] = data_off
+        return context
+
+
+class RetrospectiveUserCreateView(CreateView, LoginRequiredMixin):
+    model = RetrospectiveUser
+    fields = ['good', 'bad', 'suggestions']
+    slug_field = 'id'
+    slug_url_kwarg = 'id_retro'
+    login_url = 'login'
+    template_name = 'retrospective/retrospective_create.html'
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        try:
+            form.instance.user = self.request.user
+            form.instance.retrospective = self.model
+            return super(RetrospectiveUserCreateView, self).form_valid(form)
+        except Exception as e:
+            print e.message
+            return super(RetrospectiveUserCreateView, self).form_valid(form)
+
+
+class RetrospectiveUserEditView(UpdateView, LoginRequiredMixin):
+    model = RetrospectiveUser
+    fields = ['good', 'bad', 'suggestions']
+    slug_field = 'id'
+    slug_url_kwarg = 'id_retro'
+    login_url = 'login'
+    template_name = 'retrospective/retrospective_edit.html'
+    success_url = reverse_lazy('index')  # redirect to retrospective detail
