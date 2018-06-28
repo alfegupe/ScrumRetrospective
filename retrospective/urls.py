7e41d13ac406ec9d00a3ca315f290f94f2d2b191
jcbermudez09 <jcbermudez09@misena.edu.co>
2018-06-28 15:15:10 -0500

    adjustments in the planning view

--- retrospective/urls.py
+++ retrospective/urls.py
@@ -1,12 +1,10 @@
 # -*- encoding: utf-8 -*-
 from django.conf.urls import url
-
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name="index"),
-
     url(r'^planning/create/$',
         views.PlanningCreateView.as_view(),
         name='planning-create'),
